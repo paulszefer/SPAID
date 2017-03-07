237baee4894089655beb3308fd50a15ade2a35d0
Paul Szefer <pszefer@hotmail.com>
2017-03-06 16:41:32 -0800

    add javascript to change account tabs background color on hover

--- G13/scripts/account.js
+++ G13/scripts/account.js
@@ -1,11 +1,98 @@
+/* login tab */
+var lt = $("#logintab");
+
+/* register tab */
+var rt = $("#registertab");
+
+/* login tab selected */
+var lts = false;
+
+/* register tab selected */
+var rts = false;
+
+/* login form */
+var lf = $("#loginform");
+
+/* register form */
+var rf = $("#registerform");
+
+/* css background color property */
+var bc = "background-color";
+
+/* selected color */
+var sc = "white";
+
+/* unselected color */
+var uc = "#A2B6B8";
+
+/* css display property */
+var dp = "display";
+
+/* inline-block */
+var ib = "inline-block";
+
+lt.click(
+    function() {
+        if (!lts) {
+            lt.css(bc, sc);
+            rt.css(bc, uc);
+            lf.css(dp, ib);
+            rf.css(dp, "none");
+        } else {
+            lt.css(bc, uc);
+            rt.css(bc, uc);
+            lf.css(dp, "none");
+        }
+        lts = !lts;
+        rts = false;
+    }
+)
+
+rt.click(
+    function() {
+        if (!rts) {
+            rt.css(bc, sc);
+            lt.css(bc, uc);
+            rf.css(dp, ib);
+            lf.css(dp, "none");
+        } else {
+            rt.css(bc, uc);
+            lt.css(bc, uc);
+            rf.css(dp, "none");
+        }
+        rts = !rts;
+        lts = false;
+    }
+)
+
+lt.hover(
+    function() {
+        lt.css(bc, sc);
+    },
+    function() {
+        if (!lts) {
+            lt.css(bc, uc);
+        }
+    }
+)
+
+rt.hover(
+    function() {
+        rt.css(bc, sc);
+    },
+    function() {
+        if (!rts) {
+            rt.css(bc, uc);
+        }
+    }
+)
+
+/*
 var logintab = document.getElementById("logintab");
 var regtab = document.getElementById("registertab");
 
 var loginform = document.getElementById("loginform");
 var regform = document.getElementById("registerform");
-
-var selectedColor = "white";
-var unselectedColor = "#A2B6B8";
 
 var loginselected = false;
 var regselected = false;
@@ -14,7 +101,7 @@ logintab.onclick = function() {
     if (!loginselected) {
         changeBackgroundColor(logintab, selectedColor);
         changeBackgroundColor(regtab, unselectedColor);
-        changeDisplay(loginform, "block");
+        changeDisplay(loginform, "inline-block");
         changeDisplay(regform, "none");
     } else {
         changeBackgroundColor(regtab, unselectedColor);
@@ -30,7 +117,7 @@ regtab.onclick = function() {
         changeBackgroundColor(logintab, unselectedColor);
         changeBackgroundColor(regtab, selectedColor);
         changeDisplay(loginform, "none");
-        changeDisplay(regform, "block");
+        changeDisplay(regform, "inline-block");
     } else {
         changeBackgroundColor(logintab, unselectedColor);
         changeBackgroundColor(regtab, unselectedColor);
@@ -43,3 +130,4 @@ regtab.onclick = function() {
 function changeDisplay(obj, val) {
     obj.style.display = val;
 }
+*/
