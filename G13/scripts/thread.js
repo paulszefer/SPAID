7d8f7ff5d101ba57ae0f63f0f009ebd44a70555d
Paul Szefer <pszefer@hotmail.com>
2017-03-09 10:00:55 -0800

    add post thread/reply forms with js to display/hide them when appropriate

--- G13/scripts/thread.js
+++ G13/scripts/thread.js
@@ -1,26 +1,30 @@
 $(".rowthread").click(
     function() {
-        var selected = ($(this).find(".threadcontent").css("display") == "block");
+        var rowselected = ($(this).find(".threadcontent").css("display") == "block");
+        var postreplyactive = isPostReplyActive();
+            
+        if (!postreplyactive) {
+            $(".threadcontent").css("display", "none");
+            $(".threadauthor").css("display", "none");
+            $(".threadreplybutton").css("display", "none");
+        }
         
-        $(".threadcontent").css("display", "none");
-        $(".threadauthor").css("display", "none");
-        $(".threadreply").css("display", "none");
-        
-        if (!selected) {
+        if (!(rowselected || postreplyactive)) {
             $(this).css("background-color", "#FFFFFF");
             $(this).find(".threadauthor").css("display", "block");
             $(this).find(".threadcontent").css("display", "block");
-            $(this).find(".threadreply").css("display", "block");
-            selected = !selected;
+            $(this).find(".threadreplybutton").css("display", "block");
+            rowselected = !rowselected;
         }
     }
 )
 
 $(".rowthread").hover(
     function() {
-        var selected = ($(this).find(".threadcontent").css("display") == "block");
+        var rowselected = ($(this).find(".threadcontent").css("display") == "block");
+        var postreplyactive = isPostReplyActive();
         
-        if (!selected) {
+        if (!rowselected && !postreplyactive) {
             $(this).css("background-color", "#AAFFAA");
         }
     },
@@ -28,3 +32,52 @@ $(".rowthread").hover(
         $(this).css("background-color", "#FFFFFF");
     }
 )
+
+$("#postthreadbutton").click(
+    function() {
+        var postreplyactive = isPostReplyActive();
+        if (!postreplyactive) {
+            $("#postthreadform").css("display", "block");
+        }
+    }
+)
+
+$(".threadreplybutton").click(
+    function() {
+        var postthreadactive = isPostThreadActive();
+        if (!postthreadactive) {
+            $("#postreplyform").css("display", "block");
+        }
+    }
+)
+
+$("#submitthread").click(
+    function() {
+        $("#postthreadform").css("display", "none");
+    }
+)
+ 
+$("#submitreply").click(
+    function() {
+        $("#postreplyform").css("display", "none");
+    }
+)    
+
+$(".cancel").click(
+    function() {
+        $("#postthreadform").css("display", "none");
+        $("#postreplyform").css("display", "none");
+    }
+)
+
+function hidePost(id) {
+    $(id).css("display", "none");
+}
+
+function isPostReplyActive() {
+    return $("#postreplyform").css("display") == "block";
+}
+
+function isPostThreadActive() {
+    return $("#postthreadform").css("display") == "block";
+}
